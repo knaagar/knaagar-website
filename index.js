a6788ea5f7960ae2624beb81c197f67a39ab2728
Kritika Naagar <kritikanaagar15@gmail.com>
2022-01-07 17:48:05 +0530

    a line

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+// https://nodemailer.com/about/
 
 const app = express();
 
