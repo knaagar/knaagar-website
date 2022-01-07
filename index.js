4b8cabcf546c9d7b1d599c7d5a06887522adae61
Kritika Naagar <kritikanaagar15@gmail.com>
2022-01-07 17:35:31 +0530

    Imported code from replit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json()); 
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.use(express.static('public'));
+
+app.listen(3000, () => { 
+    console.log('Listening on port 3000') 
+})
+
+// https://replit.com/talk/learn/NodeJs-Express-tutorial/23519
+
+app.get('/', (req, res) => { 
+  res.sendFile(process.cwd() + '/public/views/index.html'); 
+});
+
+app.get('/blog', (req, res) => { 
+  res.sendFile(process.cwd() + '/public/views/blog.html'); 
+});
+
+app.get('/contact', (req, res) => { 
+  res.sendFile(process.cwd() + '/public/views/contact.html'); 
+});
+
+app.get('/milestones', (req, res) => { 
+  res.sendFile(process.cwd() + '/public/views/milestones.html'); 
+});
+
+app.get('/projects', (req, res) => { 
+  res.sendFile(process.cwd() + '/public/views/project.html'); 
+});
+
+app.get('/*', (req, res) => {
+	res.sendFile(process.cwd() + '/public/views/404.html')
+})
---
+++ public/scripts/script.js
@@ -0,0 +1,36 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+var scrollable = document.getElementById("scroll-btn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollable.style.display = "block";
+  } else {
+    scrollable.style.display = "none";
+  }
+}
+
+function scrollBtn() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// alert(window.innerWidth);
+// https://replit.com/talk/learn/NodeJs-Express-tutorial/23519
