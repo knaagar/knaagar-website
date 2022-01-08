4160de778f3ba6f65fde1b0ac3385f1978bee23c
Kritika Naagar <kritikanaagar15@gmail.com>
2022-01-08 16:43:29 +0530

    Setup basic nodemailer

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+require('dotenv').config()
 // https://nodemailer.com/about/
 
 const app = express();
@@ -37,6 +38,35 @@ app.get('/projects', (req, res) => {
   res.sendFile(process.cwd() + '/public/views/project.html'); 
 });
 
+app.get('/subscribe', (req, res) => { 
+  res.sendFile(process.cwd() + '/public/views/sub.html'); 
+});
+
 app.get('/*', (req, res) => {
 	res.sendFile(process.cwd() + '/public/views/404.html')
-})
+});
+
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL,
+//     pass: process.env.PASS,
+//   }
+// });
+
+// https://dev.to/chandrapantachhetri/sending-emails-securely-using-node-js-nodemailer-smtp-gmail-and-oauth2-g3a
+
+// var mailOptions = {
+//   from: process.env.EMAIL,
+//   to: process.env.EMAIL,
+//   subject: 'Sending Email using Node.js',
+//   text: 'That was easy!'
+// };
+
+// transporter.sendMail(mailOptions, function(error, info){
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log('Email sent: ' + info.response);
+//   }
+// });
