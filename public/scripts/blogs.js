f0c489e0a5a5d5c5565121ae1199f255a7077002
Kritika Naagar <kritikanaagar15@gmail.com>
2022-01-13 16:15:16 +0530

    Completed

--- public/scripts/blogs.js
+++ public/scripts/blogs.js
@@ -1,4 +1,27 @@
-$.getJSON('https://dev.to/api/articles/me/published', function(data) {
-    console.log(data)
+AOS.init();
+
+$.getJSON('https://dev.to/api/articles?username=knaagar', function (data) {
+    for(let i = 0; i < data.length; i++){
+        let el = document.getElementById("posts");
+        let ael = document.createElement("div");
+        let x = '/images/blog.jpg';
+        ael.innerHTML = `
+        <div class="cards" id="special">
+            <div class="img">    
+                <img src="${data[i].cover_image !== null ? data[i].cover_image : x}">
+            </div>
+            <div class="text">
+                <h2><a class="color-it" style="text-decoration:none;color:inherit;" href="${data[i].url}" target="_blank">${data[i].title}&nbsp;&nbsp;&nbsp;<i class="fa fa-external-link"></i></a></h2>
+                <p>${data[i].description.substr(0, 50)}...</p>
+                <br>
+                <p><i>Date</i>: ${data[i].readable_publish_date}
+                <br><i>Comments</i>: ${data[i].comments_count}
+                <br><i>Reactions</i>: ${data[i].public_reactions_count}
+                <br><i>Tags</i>: ${data[i].tags.replace(/ /g, '').split(',').map(s => '#' + s).join().replace(/,/g, ' ')}</p>
+            </div>
+        </div>
+        <br><br><br>
+        `;
+        el.appendChild(ael);
+    }
 });
-alert('hge')
--- public/scripts/project.js
+++ public/scripts/project.js
@@ -4,13 +4,14 @@ $.getJSON('/resources/projects.json', function(data) {
     for(let i = 0; i < data.length; i++){
         let el = document.getElementById('projects');
         let ael = document.createElement('div');
+        let x = 'https://github.com/knaagar';
         ael.innerHTML = `
         <div class="cards" data-aos="fade-up">
             <div class="img">    
                 <img src="${data[i].thumb}">
             </div>
             <div class="text">
-                <h2><a class="color-it" style="text-decoration:none;color:inherit;" href="${data[i].link}" target="_blank">${data[i].name}</a>&nbsp;&nbsp;&nbsp;<a class="color-it" style="color: #fff; font-size:1.1em;" target="_blank" href="${data[i].repo}"><i class="fa fa-github"></i></a></h2>
+                <h2><a class="color-it" style="text-decoration:none;color:inherit;" href="${data[i].link}" target="_blank">${data[i].name}</a>&nbsp;&nbsp;&nbsp;<a class="color-it" style="color: #fff; font-size:1.1em;" target="_blank" href="${data[i].repo !== '404' ? data[i].repo : x}"><i class="fa fa-github"></i></a></h2>
                 <p>${data[i].desc}</p>
             </div>
         </div>
