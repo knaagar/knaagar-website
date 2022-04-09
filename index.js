fc280ebfc751b633812d4f33f10f69c3334477fd
Kritika Naagar <kritikanaagar15@gmail.com>
2022-04-09 18:06:34 +0530

    removed req

--- index.js
+++ index.js
@@ -38,10 +38,6 @@ app.get('/projects', (req, res) => {
   res.sendFile(process.cwd() + '/public/views/project.html'); 
 });
 
-app.get('/github-repos', (req, res) => {
-	res.sendFile(process.cwd() + '/public/views/github-repos.html')
-});
-
 app.get('/*', (req, res) => {
 	res.sendFile(process.cwd() + '/public/views/404.html')
 });
