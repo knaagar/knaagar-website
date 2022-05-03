a2ad1e840feba7abcbac1c60b472b9420c6605fc
Kritika Naagar <kritikanaagar15@gmail.com>
2022-05-03 13:15:26 +0530

    Commented transporter code.

--- index.js
+++ index.js
@@ -42,47 +42,47 @@ app.get('/*', (req, res) => {
 	res.sendFile(process.cwd() + '/public/views/404.html')
 });
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASS,
-  }
-});
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL,
+//     pass: process.env.PASS,
+//   }
+// });
 
-transporter.verify(function (error, success) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("All set.");
-  }
-});
+// transporter.verify(function (error, success) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("All set.");
+//   }
+// });
 
-app.post("/send", (req, res) => {
-  let form = new multiparty.Form();
-  let data = {};
-  form.parse(req, function (err, fields) {
-    console.log(fields);
-    Object.keys(fields).forEach(function (property) {
-      data[property] = fields[property].toString();
-    });
-    console.log(data);
-    const mail = {
-      from: data.email,
-      to: process.env.EMAIL,
-      subject: data.subject,
-      text: `Sent by ${data.name} via email - ${data.email} \n${data.message}`,
-    };
-    let r = /[A-Za-z0-9$!@#%^&*]+@[A-Za-z]+\.[A-Za-z]+/g;
-    if(r.test(data.email)){
-    transporter.sendMail(mail, (err, data) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send("Something went wrong.");
-      } else {
-        res.status(200).send("Email successfully sent to recipient!");
-      }
-    });
-  }
-  });
-});
+// app.post("/send", (req, res) => {
+//   let form = new multiparty.Form();
+//   let data = {};
+//   form.parse(req, function (err, fields) {
+//     console.log(fields);
+//     Object.keys(fields).forEach(function (property) {
+//       data[property] = fields[property].toString();
+//     });
+//     console.log(data);
+//     const mail = {
+//       from: data.email,
+//       to: process.env.EMAIL,
+//       subject: data.subject,
+//       text: `Sent by ${data.name} via email - ${data.email} \n${data.message}`,
+//     };
+//     let r = /[A-Za-z0-9$!@#%^&*]+@[A-Za-z]+\.[A-Za-z]+/g;
+//     if(r.test(data.email)){
+//     transporter.sendMail(mail, (err, data) => {
+//       if (err) {
+//         console.log(err);
+//         res.status(500).send("Something went wrong.");
+//       } else {
+//         res.status(200).send("Email successfully sent to recipient!");
+//       }
+//     });
+//   }
+//   });
+// });
